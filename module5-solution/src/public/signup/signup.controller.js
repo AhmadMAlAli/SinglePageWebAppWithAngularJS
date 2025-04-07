4aa971fdb57061824930c1c3e6d3c40d39960ad9
unknown <ahmad.m.alali@outlook.com>
2025-04-07 21:23:12 +0300

    module 5

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -1,35 +1,55 @@
 (function () {
-    "use strict";
-  
-    angular.module('public')
-    .controller('SignUpController', SignUpController);
-  
-    SignUpController.$inject = ['MenuService', 'UserService'];
-    function SignUpController(MenuService, UserService) {
-      var signUpCtrl = this;
-      signUpCtrl.user = {};
-      signUpCtrl.registrationComplete = false;
-      signUpCtrl.favoriteItemError = "";
-  
-      signUpCtrl.submitForm = function () {
-        signUpCtrl.favoriteItemError = ""; // Reset error message
-        MenuService.getMenuItem(signUpCtrl.user.favorite)
+  "use strict";
+
+  angular.module('restaurant.public')
+  .controller('SignUpController', SignUpController);
+
+  SignUpController.$inject = ['MenuService', 'UserService'];
+  function SignUpController(MenuService, UserService) {
+    var signUpCtrl = this;
+    signUpCtrl.user = {};
+    signUpCtrl.registrationComplete = false;
+    signUpCtrl.favoriteItemError = "";
+    signUpCtrl.favoriteItemValid = true;
+
+    signUpCtrl.validateFavorite = function () {
+      var favorite = signUpCtrl.user.favorite;
+      if (favorite) {
+        MenuService.getMenuItem(favorite)
           .then(function (response) {
             if (response) {
-              UserService.saveUserInfo(signUpCtrl.user);
-              signUpCtrl.registrationComplete = true;
-              signUpCtrl.signUpForm.$setPristine();
-              signUpCtrl.signUpForm.$setUntouched();
-              signUpCtrl.user = {}; // Clear the form
+              signUpCtrl.signUpForm.favorite.$setValidity('noSuchItem', true);
+              signUpCtrl.favoriteItemError = "";
+              signUpCtrl.favoriteItemValid = true;
             } else {
+              signUpCtrl.signUpForm.favorite.$setValidity('noSuchItem', false);
               signUpCtrl.favoriteItemError = "No such menu number exists.";
+              signUpCtrl.favoriteItemValid = false;
             }
           })
           .catch(function (error) {
             console.error("Error checking menu item:", error);
+            signUpCtrl.signUpForm.favorite.$setValidity('noSuchItem', false);
             signUpCtrl.favoriteItemError = "Error checking menu item.";
+            signUpCtrl.favoriteItemValid = false;
           });
-      };
-    }
-  
-  })();
+      } else {
+        signUpCtrl.signUpForm.favorite.$setValidity('noSuchItem', true); // Clear error if field is empty
+        signUpCtrl.favoriteItemError = "";
+        signUpCtrl.favoriteItemValid = true;
+      }
+    };
+
+    signUpCtrl.submitForm = function () {
+      signUpCtrl.validateFavorite(); // Ensure validation runs before submit
+      if (signUpCtrl.signUpForm.$valid && signUpCtrl.favoriteItemValid) {
+        UserService.saveUserInfo(signUpCtrl.user);
+        signUpCtrl.registrationComplete = true;
+        signUpCtrl.signUpForm.$setPristine();
+        signUpCtrl.signUpForm.$setUntouched();
+        signUpCtrl.user = {}; // Clear the form
+      }
+    };
+  }
+
+})();
